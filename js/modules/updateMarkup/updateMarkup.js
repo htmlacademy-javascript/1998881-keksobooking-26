91d223da7ae3bc9007df0f91160a4728df7af948
maoltin <maoltin@gmail.com>
2022-06-21 23:44:27 +0300

    fix: new way of rendering

--- js/modules/updateMarkup/updateMarkup.js
+++ js/modules/updateMarkup/updateMarkup.js
@@ -1,7 +1,5 @@
 import { getCardData } from './../data.js';
 
-const USE_METHODS_V2 = false;
-
 const TYPE_TRANSLATIONS_OBJ = {
   flat: 'Квартира',
   bungalow: 'Бунгало',
@@ -11,7 +9,7 @@ const TYPE_TRANSLATIONS_OBJ = {
 };
 
 const injectText = (node, text, ...data) => {
-  if (data.every((item) => item)) {
+  if (data.every((dataItem) => dataItem)) {
     node.textContent = text;
   } else {
     node.remove();
@@ -19,20 +17,6 @@ const injectText = (node, text, ...data) => {
 };
 
 const addFeatures = (cardNode, features) => {
-  const featuresList = cardNode.querySelector('.popup__features');
-  featuresList.textContent = '';
-
-  const featureNodes = features.map((featureData) => {
-    const featureNode = document.createElement('li');
-    featureNode.classList.add('popup__feature', `popup__feature--${featureData}`);
-
-    return featureNode;
-  });
-
-  featuresList.append(...featureNodes);
-};
-
-const addFeaturesV2 = (cardNode, features) => {
   const featuresNodes = cardNode.querySelectorAll('.popup__feature');
 
   featuresNodes.forEach((featureNode) => {
@@ -46,21 +30,6 @@ const addFeaturesV2 = (cardNode, features) => {
 };
 
 const addPhotos = (cardNode, photosLinks) => {
-  const photosContainer = cardNode.querySelector('.popup__photos');
-  const templatePhotoNode = cardNode.querySelector('.popup__photo');
-
-  const photosNodes = photosLinks.map((photoLink) => {
-    const photoNode = templatePhotoNode.cloneNode(false);
-    photoNode.src = photoLink;
-
-    return photoNode;
-  });
-
-  templatePhotoNode.remove();
-  photosContainer.append(...photosNodes);
-};
-
-const addPhotosV2 = (cardNode, photosLinks) => {
   const photosContainer = cardNode.querySelector('.popup__photos');
   const templatePhotoNode = cardNode.querySelector('.popup__photo');
 
@@ -90,10 +59,6 @@ const getCard = (template) => {
     offer.address,
     offer.address
   );
-  
-  // const priceNode = cardNode.querySelector('.popup__text--price');
-  // const priceValueNode = priceNode.firstChild;
-  // priceValueNode.textContent = `${offer.price} `;
 
   injectText(
     cardNode.querySelector('.popup__text--price').firstChild,
@@ -106,34 +71,30 @@ const getCard = (template) => {
     TYPE_TRANSLATIONS_OBJ[offer.type],
     offer.type
   );
-  
+
   injectText(
     cardNode.querySelector('.popup__text--capacity'),
     `${offer.rooms} комнаты для ${offer.guests} гостей`,
     offer.rooms,
     offer.guests
   );
-  
+
   injectText(
     cardNode.querySelector('.popup__text--time'),
     `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`,
     offer.checkin,
     offer.checkout
   );
-  
+
   injectText(
     cardNode.querySelector('.popup__description'),
     offer.description,
     offer.description
   );
 
-  if (USE_METHODS_V2) {
-    addFeatures(cardNode, offer.features);
-    addPhotos(cardNode, offer.photos);
-  } else {
-    addFeaturesV2(cardNode, offer.features);
-    addPhotosV2(cardNode, offer.photos);
-  }
+  addFeatures(cardNode, offer.features);
+
+  addPhotos(cardNode, offer.photos);
 
   const avatar = cardNode.querySelector('.popup__avatar');
   avatar.src = author.avatar;
@@ -141,15 +102,9 @@ const getCard = (template) => {
   return cardNode;
 };
 
-const getCardsNodes = (template) => {
-  const cards = Array.from({ length: 10 }, () => getCard(template));
-
-  return cards;
-};
-
 export const updateMarkup = () => {
-  const templateFragment = document.getElementById('card').content;
-  const myCards = getCardsNodes(templateFragment);
+  const template = document.getElementById('card').content;
+  const myCards = Array.from({ length: 10 }, () => getCard(template));
   const mapCanvas = document.getElementById('map-canvas');
   mapCanvas.append(myCards[0]);
 };
