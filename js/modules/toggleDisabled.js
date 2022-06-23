36b504485b5f4fa033fc5e7cd2f311110d3d70e9
maoltin <maoltin@gmail.com>
2022-06-24 02:38:32 +0300

    fix: naming

--- js/modules/toggleDisabled.js
+++ js/modules/toggleDisabled.js
@@ -1,16 +1,16 @@
 export const getNodes = () => {
   const adFormNode = document.querySelector('.ad-form');
-  const mapfiltersNode = document.querySelector('.map__filters');
-  const children = [...adFormNode.children, ...mapfiltersNode.children];
+  const mapFiltersNode = document.querySelector('.map__filters');
+  const children = [...adFormNode.children, ...mapFiltersNode.children];
 
-  return [adFormNode, mapfiltersNode, children];
+  return [adFormNode, mapFiltersNode, children];
 };
 
 const updateClasses = (nodesToToggle, operation) => {
-  const [adFormNode, mapfiltersNode, children] = nodesToToggle;
+  const [adFormNode, mapFiltersNode, children] = nodesToToggle;
 
   adFormNode.classList[operation]('ad-form--disabled');
-  mapfiltersNode.classList[operation]('map__filters--disabled');
+  mapFiltersNode.classList[operation]('map__filters--disabled');
 
   children.forEach((node) => {
     node.classList[operation]('disabled');
